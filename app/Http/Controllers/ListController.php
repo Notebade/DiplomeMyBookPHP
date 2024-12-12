b57d5a07a4f00a9ff8bc684bb077265dc16a3bc0
mity <dimondi1010@gmail.com>
2024-12-12 22:49:32 +0300

    add user print

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Modules\Discipline\Models\Discipline;
 
 class ListController extends Controller
@@ -10,5 +11,10 @@ class ListController extends Controller
     public function disciplineShows(): \Illuminate\Database\Eloquent\Collection
     {
         return Discipline::all();
+    }
+
+    public function usersShows(): \Illuminate\Database\Eloquent\Collection
+    {
+        return User::all();
     }
 }
