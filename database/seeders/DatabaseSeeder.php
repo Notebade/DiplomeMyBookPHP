e1f20001acd3d0d76219c4c59c22ab2976e4e814
mity <dimondi1010@gmail.com>
2024-12-16 19:00:58 +0300

    add text seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             DisciplineBase::class,
             SubjectBase::class,
             BaseTheme::class,
+            TextBase::class,
         ]);
     }
 }
