8bbb89a8f7d187b2f2e451bc0a9bbe3d845ee6ab
mity <dimondi1010@gmail.com>
2025-01-19 11:41:19 +0300

    update test models

--- app/Modules/Test/Models/Test.php
+++ app/Modules/Test/Models/Test.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace App\Modules\Test\Models;
 
+use App\Modules\Theme\Models\Theme;
+use App\Modules\User\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -20,16 +23,21 @@ class Test extends Model
         'theme_id',
         'code',
         'name',
+        'user_id',
     ];
 
     protected $hidden = [
         'created_at',
         'updated_at',
         'deleted_at',
+        'user_id',
+        'theme_id',
     ];
 
     protected $appends = [
-      'questions',
+        'questions',
+        'user',
+        'theme'
     ];
 
     public function questions(): HasMany
@@ -37,8 +45,28 @@ class Test extends Model
         return $this->hasMany(Questions::class, 'test_id', 'id');
     }
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function theme(): BelongsTo
+    {
+        return $this->belongsTo(Theme::class, 'theme_id', 'id');
+    }
+
     public function getQuestionsAttribute(): mixed
     {
         return $this->questions()->get();
+    }
+
+    public function getUserAttribute(): mixed
+    {
+        return $this->user()->first();
+    }
+
+    public function getThemeAttribute(): mixed
+    {
+        return $this->theme()->first();
     }
 }
